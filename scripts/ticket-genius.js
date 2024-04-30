8bfa3110bb3fb82dd53ff32a5bb2884f9bd6c6ee
mirazfaruk <mirazfaruk@gmail.com>
2024-04-30 12:59:05 +0600

    Added the function where selected seats color change and update the current seats number

--- scripts/ticket-genius.js
+++ scripts/ticket-genius.js
@@ -3,3 +3,34 @@ function buyTickets() {
     toggleMainScreenVisibility();
     scrollToMainScreen();
 }
+
+// Get all seat buttons
+const seatButtons = document.querySelectorAll('.seat-button');
+const selectedSeats = [];
+
+seatButtons.forEach(seatButton => {
+    seatButton.addEventListener('click', function () {
+        // Check if the selectedSeats array length is less than or equal to 4
+        if (selectedSeats.length < 4) {
+            // Toggle the color of the button and update current seats
+            if (!selectedSeats.includes(this.id)) {
+                this.style.backgroundColor = '#1DD100';
+                selectedSeats.push(this.id);
+                const currentSeats = getTextElementValueById('current-seats');
+                const updatedSeats = currentSeats - 1;
+                setTextElementValueById('current-seats', updatedSeats);
+            } else {
+                this.style.backgroundColor = '';
+                selectedSeats.splice(selectedSeats.indexOf(this.id), 1);
+                const currentSeats = getTextElementValueById('current-seats');
+                const updatedSeats = currentSeats + 1;
+                setTextElementValueById('current-seats', updatedSeats);
+            }
+
+            console.log('Selected seats:', selectedSeats);
+        } else {
+            // Message if the maximum number of seats is reached
+            alert('You have already selected the maximum number of seats (4).');
+        }
+    });
+});
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,15 @@ function scrollToMainScreen() {
     const mainScreen = document.getElementById('main-screen');
     mainScreen.scrollIntoView({ behavior: 'smooth' });
 }
+
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
